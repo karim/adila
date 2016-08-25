6ab57058e1f748acd09c2ffb28c94827e1e977ac
Karim ElDeeb <keldeeb@gmail.com>
2016-08-25 23:56:46 +0200

    Update database

--- database/src/main/java/adila/db/bno5fmt5593uplus5feu_qm153e.java
+++ database/src/main/java/adila/db/bno5fmt5593uplus5feu_qm153e.java
@@ -3,12 +3,12 @@
 package adila.db;
 
 /*
- * Bang & Olufsen BeoVision 14
+ * Bang & Olufsen BeoVision
  *
  * DEVICE: bno_MT5593Uplus_EU
  * MODEL: QM153E
  */
 final class bno5fmt5593uplus5feu_qm153e {
     public static final String MANUFACTURER = "Bang & Olufsen";
-    public static final String NAME = "BeoVision 14";
+    public static final String NAME = "BeoVision";
 }
